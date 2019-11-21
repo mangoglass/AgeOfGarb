9d7a0c1fa2a3d66c2d413a25d1f30387e97a15ab
Schytheron <12500415+Schytheron@users.noreply.github.com>
2019-11-20 19:29:51 -0800

    Added paint splatter system basics (1/2)
    Still needs some tweaking and minor fixes.

--- Assets/Scripts/CamScript.cs
+++ Assets/Scripts/CamScript.cs
@@ -26,5 +26,13 @@ public class CamScript : MonoBehaviour
                 rb.AddForce(cam.transform.forward * 20f, ForceMode.Impulse);
             }    
         }
+
+        // This part is only used for quick dev testing on PC
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject projectile = Instantiate(projectileClass, cam.transform.position + (cam.transform.forward), cam.transform.rotation);
+            Rigidbody rb = projectile.GetComponent<Rigidbody>();
+            rb.AddForce(cam.transform.forward * 20f, ForceMode.Impulse);
+        }
     }
 }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,23 +4,36 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    public GameObject decal;
+    private SphereCollider collider;
+    private Rigidbody rb;
+    private RaycastHit hit;
+
+    void Awake()
+    {
+        Destroy(gameObject, 10f);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        collider = GetComponent<SphereCollider>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Physics.Raycast(transform.position, rb.velocity.normalized, out hit, collider.radius + 0.1f))
+        {
+            Quaternion decalRotation = Quaternion.LookRotation(hit.normal);
+            decalRotation *= Quaternion.Euler(0, 180f, 0);
+            Instantiate(decal, hit.point, decalRotation);
+            print(hit.transform.gameObject.name);
+            Renderer otherRenderer = hit.transform.gameObject.GetComponent<Renderer>();
+            otherRenderer.material.SetColor("_Color", Color.red);
+            Destroy(gameObject);
+        }
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        print(other.gameObject.name);
-        Renderer otherRenderer = other.gameObject.GetComponent<Renderer>();
-        otherRenderer.material.SetColor("_Color", Color.red);
-        Destroy(gameObject);
-    }
 }
