54749cc99d879c8abc1c9cc35f4c90aa6a0fc6d5
Natalie Axelsson <nataxe@kth.se>
2019-12-12 17:23:02 +0100

    Added progress bar prefab

---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Instructions:
+ *
+ * public class ProgressBarUser {
+ *      [SerializeField]
+ *      private GameObject progressBarPrefab;
+ *      private ProgressBar progressBar;
+ *
+ *      public void Start() {
+ *          GameObject newProgressBar = Instantiate(progressBarPrefab, parent.transform);
+ *          newProgressBar.transform.localPosition = new Vector3(0, 5, 0);
+ *          ProgressBar progressBar = newProgressBar.GetComponentInChildren<ProgressBar>();
+ *          progressBar.MaxValue = 20f;
+ *      }
+ *
+ *      public void OnProgressUpdate(float newValue) {
+ *          progressBar.CurrentValue = newValue;
+ *      }
+ *  }
+ */
+
+public class ProgressBar : MonoBehaviour
+{
+    private UnityEngine.UI.Slider slider;
+    private Transform camera;
+
+    private float maxValue;
+    public float MaxValue {
+        get {
+            return maxValue;
+        }
+        set {
+            maxValue = value;
+            slider.value = currentValue / maxValue;
+        }
+    }
+
+    private float currentValue;
+    public float CurrentValue {
+        get {
+            return currentValue;
+        }
+        set {
+            currentValue = value;
+            slider.value = currentValue / maxValue;
+        }
+    }
+
+    private void Start() {
+        camera = Camera.main.transform;
+    }
+
+    private void Awake() {
+        slider = gameObject.GetComponent<UnityEngine.UI.Slider>();
+        currentValue = 0;
+        maxValue = 100;
+    }
+
+    private void Update() {
+        transform.LookAt(2f * transform.position - camera.position);
+    }
+}
